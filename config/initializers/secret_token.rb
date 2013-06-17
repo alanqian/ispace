f7ba8006f642a096ec93c99efe8553776951cdaf
gqian <gqian@avos.com>
2013-06-18 02:01:19 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ispace::Application.config.secret_key_base = '13ea8b9bd92223d87c34f00d14d552841e524d4cf0b603e320059e1c81d4fe46d4644e8fcd95842899439380d2089b66d22a6ee87079ff589c51ba68fe0f6965'
