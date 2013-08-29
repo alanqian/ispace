4ca769a164d40b2476ecdd45451e5bafd0ff9ed3
alanqian <qianguoxiang2008@avos.com>
2013-08-29 18:10:30 +0800

    add helpers: data_th_list, data_td_list; tf(xxx_id)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,14 @@ module ApplicationHelper
       s = I18n.t("#{prefix}.#{field}", default: '')
       return s unless s.empty?
     end
+    # check field: xxx_id
+    if field =~ /_id$/
+      f = field.to_s.sub(/_id$/, "")
+      prefixs.each do |prefix|
+        s = I18n.t("#{prefix}.#{f}", default: '')
+        return s unless s.empty?
+      end
+    end
     return I18n.t("#{prefixs[0]}.#{field}")
   end
 
@@ -32,20 +40,41 @@ module ApplicationHelper
   end
 
   def select_one_check(id, sel_target, opts = {})
+    opts ||= {}
     opts[:onclick] ||= "javascript: onClickSelectOne(event, this);"
     opts[:data] ||= {}
     opts[:id] ||= "#{sel_target.gsub(/s?\[\]/, "_")}#{id}"
     opts[:data][:select_all] ||= "#SELECT_ALL"
+    if opts.has_key?(:class)
+      opts[:class] = "select-one #{opts[:class]}"
+    else
+      opts[:class] = "select-one"
+    end
     check_box_tag(sel_target, id, false, opts)
   end
 
   def select_all_check(sel_target, opts = {})
+    opts ||= {}
     opts[:onclick] ||= "javascript: onClickSelectAll(event, this);"
     opts[:data] ||= {}
     opts[:data][:target] ||= sel_target
-    check_box_tag(:SELECT_ALL, "1", false, opts)
+    if opts.has_key?(:class)
+      opts[:class] = "select-all #{opts[:class]}"
+    else
+      opts[:class] = "select-all"
+    end
+    check_box_tag(:SELECT_ALL, "1", false, opts) + content_tag("span", "", class: "hint")
   end
 
+  # data_array syntax:
+  #   :field|value, { input:false, sort:false show:false }
+  # eg:
+  #   :select_all, "products[]", ...
+  #   :field
+  #   :field, input:false|name, search:false, sort:false|a|d, show:false, ...
+  #    value
+  #    value, input:false|name, search:false, sort:false|a|d, show:false, ...
+  #
   # :field                w/ default input, sort, search
   #       <th data-input="product[code]"><%=tf :code %></th>
   #  value                input:nil, w/ default sort, search
@@ -56,25 +85,106 @@ module ApplicationHelper
   #       see :field
   #  value,  input:?, sort:, search:, show:
   #       see :field
-  def data_th_list(object_sym, data_array)
+  def data_th_list(object_sym, *data_array)
     object = object_sym.to_s.downcase
+    ths = []
     data_array.each do |col|
       if col.is_a?(Array)
+        logger.debug "data col: #{col}"
         # w/ opts
-        field = col[0]
-        opts = col[1]
-        if col == :select_all
-          opts = { no_sort: "1", no_search: "1"}
+        field = col.shift
+        if field == :select_all
+          sel_target, sel_opts = col
+          sel_opts ||= {}
+          opts = { no_input: "1", no_sort: "1", no_search: "1"}
+          content = select_all_check(sel_target, sel_opts)
+        else
+          if field.is_a?(Symbol)
+            opt = col.shift || {}
+            opts = { input: "#{object}[#{field}]" }.merge(opt)
+            content = tf(field)
+          else
+            opts = col.shift || {}
+            content = field
+          end
+          # normalize opts: input:, sort:, search:, show:
+          # change :false to no_xxx
+          [:input, :sort, :search, :show].each do |k|
+            if opts.has_key?(k)
+              v = opts[k]
+              if v.kind_of?(FalseClass)
+                opts.delete(k)
+                opts["no_#{k}".to_sym] = 1
+              end
+            end
+          end
         end
-      else
-        # w/o opts
+      else # scalar, w/o opts
         field = col
         if col.is_a?(Symbol)
-          opts = { input: ""}
+          # :field => w/ default input, sort, search
+          opts = { input: "#{object}[#{field}]" }
+          content = tf(field)
         else
-          opts = { no_sort: "1", no_search: "1"}
+          # input:nil, w/ default sort, search
+          opts = {}
+          content = col.to_s
         end
       end
+      [:input,:no_input, :sort,:no_sort, :search,:no_search, :show,:no_show].each do |k|
+        if opts.has_key?(k)
+          v = opts.delete(k)
+          opts[:data] ||= {}
+          opts[:data][k] = v
+        end
+      end
+      ths.push content_tag(:th, content, opts)
     end
+    raw(ths.join("\n"))
+  end
+
+  # data_array syntax:
+  #   [:select_one, :id-field, "sel-group"],
+  #   [:field, collection: coll],
+  #   [:field, content-value],
+  #   :field,
+  #   content-value,
+  def data_td_list(object, *data_array)
+    tds = []
+    data_array.each do |col|
+      opts = nil
+      if col.is_a?(Array)
+        # [:select_one, :code, "products[]"],
+        # [:color, color_tag(product.color)],
+        # [:brand_id, collection: brands_hash],
+        t = col.shift
+        if t == :select_one
+          id, sel_target = col.shift(2)
+          # test if it's a field?
+          id = object.send(id) if id.is_a?(Symbol)
+          content = select_one_check(id, sel_target)
+        else
+          field = t
+          val = object.send(field)
+          opts = { data: {val: val}}
+          t = col.shift
+          if t.is_a?(Hash)
+            coll = t[:collection]
+            content = coll[val]
+          else
+            content = t.to_s
+          end
+        end
+      elsif col.is_a?(Symbol)
+        # :name,
+        field = col
+        content = object.send(field)
+      else
+        # simply pure value
+        content = col
+      end
+      tds.push content_tag(:td, content, opts)
+    end
+    raw(tds.join("\n"))
   end
 end
