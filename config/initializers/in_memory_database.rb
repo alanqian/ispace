71c1504950175e57a24e263063728497b98241fa
alanqian <qianguoxiang2008@avos.com>
2013-09-01 10:31:32 +0800

    setup test env database: sqlite :memory:

---
+++ config/initializers/in_memory_database.rb
@@ -0,0 +1,19 @@
+=begin database.yml
+  test:
+    adapter: sqlite3
+    database: ":memory:"
+    verbosity: quiet
+=end
+
+def in_memory_database?
+  Rails.env == "test" and
+    # ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLiteAdapter ||
+    ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter and
+    Rails.configuration.database_configuration['test']['database'] == ':memory:'
+end
+
+if in_memory_database?
+  puts "creating sqlite in memory database"
+  load "#{Rails.root}/db/schema.rb"
+end
+
