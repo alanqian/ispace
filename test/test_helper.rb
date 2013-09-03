c5a96dd4f9ca887f0db5bc878a638aca27b8a50c
alanqian <qianguoxiang2008@avos.com>
2013-09-03 16:18:17 +0800

    test_helper: add to_params,to_nested_params,etc

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,46 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActiveRecord::Base
+  def to_params(excepts=[], rep_hash={})
+    excepts.push "created_at", "updated_at"
+    params = self.attributes
+    excepts.each { |f| params.delete f }
+    params.merge! rep_hash
+    params
+  end
+
+  def to_new_params(excepts=["id"], rep_hash={})
+    to_params(excepts, rep_hash)
+  end
+end
+
+class Array
+  def to_nested_params
+    # open_shelves_attributes => { id => parameters_except_id }
+    params = {}
+    index = 0
+    new_index = self.size + 100
+    self.each do |r|
+      if r.id
+        params[index] = r.to_params
+        index += 1
+      else
+        params[new_index] = r.to_params
+        new_index += 1
+      end
+    end
+    params
+  end
+
+  def to_new_nested_params
+    params = {}
+    index = self.size + 100
+    self.each do |r|
+      params[index] = r.to_new_params
+      index += 1
+    end
+    params
+  end
+end
