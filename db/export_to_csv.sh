3285db0c8d0e82f92cc6583543e972c853540ca1
aisensiy <aisensiy@gmail.com>
2013-10-21 12:48:22 +0800

    rm \N in csv

--- db/export_to_csv.sh
+++ db/export_to_csv.sh
@@ -13,6 +13,11 @@ fi
 
 mysql ispace_dev < to_csv.sql
 
+sed -i '' 's/\\N//g' /tmp/sales.csv
+sed -i '' 's/\\N//g' /tmp/products.csv
+sed -i '' 's/\\N//g' /tmp/categories.csv
+sed -i '' 's/\\N//g' /tmp/stores.csv
+
 mv /tmp/stores.csv ./csvs
 mv /tmp/categories.csv ./csvs
 mv /tmp/products.csv ./csvs
