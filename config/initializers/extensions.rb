5923dd7cf929d163cb457f4bffbb1bb9ddac9d78
alanqian <qianguoxiang2008@gmail.com>
2013-10-22 09:30:13 +0800

    add simple string template

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -1,1 +1,2 @@
 require "active_record_extension"
+require "template"
---
+++ lib/template.rb
@@ -0,0 +1,56 @@
+module Template
+  # templates:
+  #   #{foo}
+  #   #{foo.bar}
+  #   #{foo[baz]}
+  #   #{foo[baz].bar}
+  TemplateRE = /\#\{
+    (?<foo>[^\.\[\]\}]+)      # foo
+    (\[(?<baz>[^\[\]\}]+)\])? # baz
+    (\.(?<bar>[\w\?]+))?      # bar
+    \}/x
+  def template(args = {})
+    self.gsub(TemplateRE) do |match|
+      foo = $~[:foo]
+      baz = $~[:baz]
+      if baz && baz.start_with?(":")
+        baz = baz.delete(":").to_sym
+      end
+      bar = $~[:bar]
+      #puts "foo:#{foo} baz:#{baz} bar:#{bar}"
+      if args[foo]
+        val = args[foo]
+        if baz
+          val = val.is_a?(Array) ? val[baz.to_i] : val[baz]
+        end
+        if bar
+          val = val.send(bar)
+        end
+        val.to_s
+      else
+        match
+      end
+    end
+  end
+end
+
+# include the extension
+String.send(:include, Template)
+
+__END__
+
+baz1 = Struct.new(:bar).new("baz1")
+baz2 = Struct.new(:bar).new("bar2")
+foo = {
+  baz: baz1
+}
+
+s = '#{baz} #{baz.bar} #{foo[:baz]} #{foo[:baz].bar}'
+
+puts s
+puts "-->"
+puts s.template("foo" => foo, "baz" => baz1 )
+s = '#{foo[0]} #{foo[1].bar}'
+foo = [baz1, baz2]
+puts s.template("foo" => foo, "baz" => baz2 )
+
