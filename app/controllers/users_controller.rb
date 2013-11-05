cd7fa1edf86fdf983fa5fa86dc81a3974a67cce7
aisensiy <aisensiy@gmail.com>
2013-11-05 20:32:30 +0800

    add create user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def new
+    @user = User.new
+  end
+
   def edit
   end
 
@@ -25,7 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
       else
         format.html { render action: 'new' }
       end
@@ -56,6 +60,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :employee_id, :email, :telephone)
+    params.require(:user).permit(:username, :employee_id, :email, :telephone, :password, :password_confirmation)
   end
 end
