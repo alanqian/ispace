500ccbf12828debadce009f0fedc3aecb091ad5e
aisensiy <aisensiy@gmail.com>
2013-11-05 20:33:07 +0800

    add test for user create

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  fixtures :users
+
+  setup do
+    @user_attrs = {
+      username: 'test_attr',
+      email: 'test_email@g.cn',
+      password: '00000000',
+      password_confirmation: '00000000'
+    }
+  end
+
   test "should get show" do
   end
 
@@ -11,6 +22,10 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should get create" do
+    assert_difference "User.count", 1 do
+      post :create, user: @user_attrs
+    end
+    assert_redirected_to users_path
   end
 
   test "should get destroy" do
