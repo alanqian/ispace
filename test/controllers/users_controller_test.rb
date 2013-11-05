c9e72c10850a645171037566e07f0f72fe7f4845
aisensiy <aisensiy@gmail.com>
2013-11-05 20:38:38 +0800

    make employee id and username uniqueness

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,7 @@ class UsersControllerTest < ActionController::TestCase
   setup do
     @user_attrs = {
       username: 'test_attr',
+      employee_id: '10088',
       email: 'test_email@g.cn',
       password: '00000000',
       password_confirmation: '00000000'
