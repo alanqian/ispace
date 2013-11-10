8bb330b2f304de16e195065b3b1c695ef4f0a1b2
aisensiy <aisensiy@gmail.com>
2013-11-11 01:35:17 +0800

    add general flash content for in application.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -243,4 +243,25 @@ module ApplicationHelper
     end
     raw(tds.join("\n"))
   end
+
+  def flash_contents
+    alert_types = [:error, :info, :success, :warning]
+
+    flash_messages = []
+    flash.each do |type, message|
+      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
+      next if message.blank?
+
+      type = :success if type == :notice
+      type = :error   if type == :alert
+      next unless alert_types.include?(type)
+
+      Array(message).each do |msg|
+        text = content_tag(:p, msg.html_safe, :id => "notice", :class => "alert fade in alert-#{type}")
+        flash_messages << text if msg
+      end
+    end
+    flash_messages.join("\n").html_safe
+  end
+
 end
