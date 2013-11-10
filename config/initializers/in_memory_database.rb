e0bb5cccd3c5a1e9a23f7c70250d0de0d1106a0b
alanqian <qianguoxiang2008@gmail.com>
2013-11-10 15:58:37 +0800

    fix in_memory_database bug and fixtures/_item

--- config/initializers/in_memory_database.rb
+++ config/initializers/in_memory_database.rb
@@ -7,9 +7,9 @@
 
 def in_memory_database?
   Rails.env == "test" and
-    # ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLiteAdapter ||
-    ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter and
-    Rails.configuration.database_configuration['test']['database'] == ':memory:'
+    Rails.configuration.database_configuration['test']['database'] == ':memory:' and
+    (ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLiteAdapter ||
+    ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter)
 end
 
 if in_memory_database?
