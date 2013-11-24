68268ce9512814707649761c44ea303c181cc4b1
alanqian <qianguoxiang2008@gmail.com>
2013-11-24 15:35:08 +0800

    tree-input: add data-url options for category input

--- app/inputs/tree_input.rb
+++ app/inputs/tree_input.rb
@@ -2,12 +2,15 @@
 class TreeInput < SimpleForm::Inputs::TextInput
   def input
     input_html_options[:data] ||= {}
+    options[:data] ||= {}
+    id_data_options = options[:data].merge(type: attribute_name)
+
     cmd = options.delete :cmd
     input_html_options[:data][:cmd] = cmd
     input_html_options[:data][:type] = attribute_name
     input_html_options.delete :cmd
 
-    @builder.hidden_field(attribute_name, "data-type" => attribute_name) +
+    @builder.hidden_field(attribute_name, data: id_data_options) +
       @builder.text_field(label_target, input_html_options)
   end
 
