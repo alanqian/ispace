f813b028c88d20711b8c80196a263b438767ea0f
alanqian <qianguoxiang2008@gmail.com>
2013-12-03 16:28:50 +0800

    random_color: fixed typo error

--- app/models/concerns/random_color.rb
+++ app/models/concerns/random_color.rb
@@ -6,8 +6,8 @@ module RandomColor
       options = colors.shuffle
       count = 0
       pkey = self.primary_key.to_sym
-      self.select(pkey, :category_id, :color).order(:category_id).each do |brand|
-        brand.update_column(:color, options[count % options.size])
+      self.select(pkey, :category_id, :color).order(:category_id).each do |ar|
+        ar.update_column(:color, options[count % options.size])
         count += 1
       end
       count
